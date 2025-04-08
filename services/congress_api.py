276b9696d6a89d1ed213e90818f38acd3895a6a5
EnUrbe <96707624+EnUrbe@users.noreply.github.com>
2025-04-08 15:34:29 -0600

    Update congress_api.py

--- services/congress_api.py
+++ services/congress_api.py
@@ -1,1 +1,15 @@
+import requests
+
+def search_federal_bills(keyword):
+    url = "https://www.govtrack.us/api/v2/bill"
+    params = {
+        "q": keyword,
+        "sort": "-introduced_date",
+        "limit": 5
+    }
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        return response.json()["objects"]
+    else:
+        return {"error": response.text}
 
