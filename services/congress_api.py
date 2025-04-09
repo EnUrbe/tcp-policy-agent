cbfb65ded978947b23a3549d84e21e0c4af599f7
EnUrbe <96707624+EnUrbe@users.noreply.github.com>
2025-04-08 20:58:09 -0600

    Update congress_api.py

--- services/congress_api.py
+++ services/congress_api.py
@@ -1,6 +1,9 @@
 import requests
 
 def search_federal_bills(keyword):
+    """
+    Returns top 5 bills from GovTrack that match 'keyword'.
+    """
     url = "https://www.govtrack.us/api/v2/bill"
     params = {
         "q": keyword,
@@ -9,7 +12,7 @@ def search_federal_bills(keyword):
     }
     response = requests.get(url, params=params)
     if response.status_code == 200:
-        return response.json()["objects"]
+        data = response.json()
+        return data.get("objects", [])
     else:
         return {"error": response.text}
-
